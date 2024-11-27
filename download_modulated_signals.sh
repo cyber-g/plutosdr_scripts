c8c4fcaa67440af82462af925d60b9836a675d42
Cyber-G <cygerpham@free.fr>
2024-11-27 23:37:53 +0100

    download_modulated_signals.sh: add protected signal

--- download_modulated_signals.sh
+++ download_modulated_signals.sh
@@ -14,3 +14,12 @@ for file in $FILES; do
     wget -c "$URL_BASE$file" -O signals/$file
 done
 
+### Get protected signals
+
+# Load the access token stored in the file token.txt
+TOKEN=$(cat token.txt)
+# Get the file from https://gitlab.telecom-paris.fr/c2s/enseignement/smart-ics902/dpd-lab/-/raw/main/signals/LTE5_61p44Msps_PAR7p5dB.mat
+curl --header "PRIVATE-TOKEN: $TOKEN" \
+https://gitlab.telecom-paris.fr/c2s/enseignement/smart-ics902/dpd-lab/-/raw/main/signals/LTE5_61p44Msps_PAR7p5dB.mat \
+-o signals/LTE5_61p44Msps_PAR7p5dB.mat
+
